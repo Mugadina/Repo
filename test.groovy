f4a845ac627522af5c2d40b49d95783d971c6fb0
Mugadina <mugad2017@yandex.ru>
2022-05-02 23:22:20 +0300

    sdsadsadsadasdsad

--- test.groovy
+++ test.groovy
@@ -1,12 +1,9 @@
 pipeline {
     agent any
     deleteDir()
-    final String bitbucketCredentialId = "github"
-    final String workDir = pwd()
-    String nexusType, urlBitbucketRepository
     git(
             branch: "master",
-            credentialsId: "${bitbucketCredentialId}",
+            credentialsId: "github",
             url: "https://github.com/Mugadina/Repo.git"
     )
 }    
