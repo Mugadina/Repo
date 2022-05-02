1b3ec39120ed6fbb4775d4c7c7a2e08e14411178
Mugadina <mugad2017@yandex.ru>
2022-05-02 23:12:29 +0300

    s1231313

--- test.groovy
+++ test.groovy
@@ -3,9 +3,6 @@ node("any") {
     final String bitbucketCredentialId = "Gigimon"
     final String workDir = pwd()
     String nexusType, urlBitbucketRepository
-    if ("https://sbt-jenkins.sigma.sbrf.ru/".equals(env.JENKINS_URL)) {
-        println("[INFO] Was chosen EDZ stand " + env.JENKINS_URL.toString())
-        urlBitbucketRepository = "https://sbrf-bitbucket.sigma.sbrf.ru/scm/ci02503964/ci02505473_global_jobs.git"
     git(
             branch: "master",
             credentialsId: "${bitbucketCredentialId}",
