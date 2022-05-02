5f00dca697b105815df217030485d92336f535c6
Mugadina <mugad2017@yandex.ru>
2022-05-02 23:37:24 +0300

    sdsadsadsadasdsad

--- test.groovy
+++ test.groovy
@@ -19,6 +19,7 @@ pipeline {
             	   url: "https://github.com/WebGoat/WebGoat.git"
                )
                sh "ls -la"
+               sh "docker run -it -p 127.0.0.1:8080:8080 -p 127.0.0.1:9090:9090 -e TZ=Europe/Amsterdam webgoat/webgoat"
 	    }
 	    
     	
