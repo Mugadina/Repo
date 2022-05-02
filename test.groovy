026f4d2864e93e2278c32f9bbdf2a47b9e94f4ca
Mugadina <mugad2017@yandex.ru>
2022-05-02 23:56:40 +0300

    sdsadsadsadasdsad

--- test.groovy
+++ test.groovy
@@ -23,7 +23,12 @@ pipeline {
                sh "ls -la"
                sh "java -Dfile.encoding=UTF-8 -jar webgoat-8.2.3.jar"
                sh "sudo chmod 700 ./webgoat-8.2.3.jar "
-               sh "sudo rm ./*"
+               //sh "sudo rm ./*"
+               post {
+                  always {
+                      cleanWs()
+                  }
+               }
                //sh "docker run -it -p 127.0.0.1:8080:8080 -p 127.0.0.1:9090:9090 -e TZ=Europe/Amsterdam webgoat/webgoat"
 	    }
 	    
