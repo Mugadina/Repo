7cac85c567edab36a0d7ed89ee9f66f7dcb99be5
Mugadina <mugad2017@yandex.ru>
2022-05-03 14:28:21 +0300

    sdsadsadsadasdsad

--- test.groovy
+++ test.groovy
@@ -16,19 +16,15 @@ pipeline {
 	    	git(
             	   branch: "develop",
             	   credentialsId: "github",
-            	   url: "https://github.com/WebGoat/WebGoat.git"
+            	   url: "git@github.com:WebGoat/WebGoat.git"
                )
-               //sh "ls -la"
+               //sh  "ls -la"
                //sh "wget https://github.com/WebGoat/WebGoat/releases/download/v8.2.2/webgoat-server-8.2.2.jar"
+               sh  "./mvnw clean install"
                sh "ls -la"
                //sh "java -Dfile.encoding=UTF-8 -jar webgoat-8.2.3.jar"
                //sh "echo status97 | sudo -S command chmod 700 ./webgoat-8.2.3.jar"
                //sh "sudo rm ./*"
-               post {
-                  always {
-                      cleanWs()
-                  }
-               }
                //sh "docker run -it -p 127.0.0.1:8080:8080 -p 127.0.0.1:9090:9090 -e TZ=Europe/Amsterdam webgoat/webgoat"
 	    }
 	    
