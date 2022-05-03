ffe2084d923c767beaf968c436f7f996d8668cb2
Mugadina <mugad2017@yandex.ru>
2022-05-03 14:36:03 +0300

    sdsadsadsadasdsad

--- test.groovy
+++ test.groovy
@@ -20,8 +20,12 @@ pipeline {
                )
                //sh  "ls -la"
                //sh "wget https://github.com/WebGoat/WebGoat/releases/download/v8.2.2/webgoat-server-8.2.2.jar"
-               sh  "./mvnw clean install -e"
+               
+               sh "java --version"
+               //sh  "./mvnw clean install -e"
                sh "ls -la"
+               
+               
                //sh "java -Dfile.encoding=UTF-8 -jar webgoat-8.2.3.jar"
                //sh "echo status97 | sudo -S command chmod 700 ./webgoat-8.2.3.jar"
                //sh "sudo rm ./*"
